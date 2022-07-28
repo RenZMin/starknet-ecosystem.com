57b52f33ac65fb37551e6d39296fe30d48e18352
RenZMin <42882641+RenZMin@users.noreply.github.com>
2022-07-29 02:05:59 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-   
-    // default language
-    defaultLocale: 'en',
-  },
+
 }
 
 module.exports = nextConfig
